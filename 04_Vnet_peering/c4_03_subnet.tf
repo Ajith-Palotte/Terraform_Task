d9507e8387a6c97a48777a04374eeb384ce13006
Ajith-Palotte <ajithpalotte@hotmail.com>
2022-02-06 19:45:35 +0530

    network_peering_mgmt-app

---
+++ 04_Vnet_peering/c4_03_subnet.tf
@@ -0,0 +1,18 @@
+# MGMT subnet
+
+resource "azurerm_subnet" "mgmt_subnet" {
+    name = "${local.resource_name_prefix}-${var.mgmt_subnet_name}"
+    resource_group_name = azurerm_resource_group.RG1.name
+virtualvirtual_network_name = azurerm_virtual_network.mgmt_vnet.name
+address_priaddress_prefixes = var.mgmt_subnet_address   
+  
+}
+
+# app subnet creation
+
+resource "azurerm_subnet" "app_subnet" {
+  name ="${local.resource_name_prefix}=${var.app_subnet_name}"
+  resource_group_name = azurerm_resource_group.RG1.name
+  virtual_network_name = azurerm_virtual_network.app_vnet.name
+  address_prefixes = var.app_subnet_address
+}
